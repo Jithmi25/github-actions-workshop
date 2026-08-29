9703482a675c7e0cc6de9ce37da90f5616e20536
Wickramasinghe W.M.J.P. <wickjitha@gmail.com>
2026-08-29 23:29:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,10 +11,8 @@ function multiply(a, b) {
 // TASK FOR PARTICIPANTS:
 // This function has a bug! It should subtract b from a,
 // but right now it adds them instead.
-// During the session, fix this line, commit, push,
-// and watch the GitHub Actions workflow re-run and pass.
 function subtract(a, b) {
-  return a - b; // BUG: should be "return a - b;"
+  return a + b; // BUG: should be "return a - b;"
 }
 
 module.exports = { add, multiply, subtract };
